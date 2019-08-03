516a9c6b8571b18368300f6fe4019e20b03528cc
Ballu123 <balram.panwar@gmail.com>
2019-08-02 22:52:31 -0700

    Update Virtualconcept.cpp

--- Virtualconcept.cpp
+++ Virtualconcept.cpp
@@ -19,34 +19,34 @@ class D: public B
  
 int main() {
 B b;
-b.func(); //This is class B
+b.func();        //This is class B
 
 B b1;
 B &b2 = b1;
-b2.func();	//This is class B
+b2.func();	 //This is class B
 
 B *b3 = new B;
-b3->func(); //This is class B
+b3->func();      //This is class B
 
 D d;
-d.func(); //This is class D
+d.func();       //This is class D
 	
 D d1;
 D &d2 = d1;
-d2.func(); //This is class D
+d2.func();       //This is class D
 	
 D *d3 = new D;
-d3->func(); //This is class D
+d3->func();      //This is class D
 
 D d4;
 B bnew = d4;
-bnew.func();	//This is class B
+bnew.func();	 //This is class B
 
 D d5;
 B &b_1 = d5;
-b_1.func(); //This is class D
+b_1.func();      //This is class D
 
 B *b7 = new D;
-b7->func(); //This is class D
+b7->func();     //This is class D
 return 0;
 }

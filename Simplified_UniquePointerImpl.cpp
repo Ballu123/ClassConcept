f36afc2d806ee8a890dcd26015ecec917951a26b
Ballu123 <balram.panwar@gmail.com>
2024-08-17 11:28:59 -0700

    Update Simplified_UniquePointerImpl.cpp

--- Simplified_UniquePointerImpl.cpp
+++ Simplified_UniquePointerImpl.cpp
@@ -44,48 +44,63 @@ public:
 
     unique_ptr& operator=(unique_ptr&& other) noexcept {
         if (this != &other) {
-            delete ptr_;
-            ptr_ = other.ptr_;
+            delete ptr_; // Delete the current resource
+            ptr_ = other.ptr_;  // Transfer ownership
             other.ptr_ = nullptr;
         }
         return *this;
     }
 
-    // Member functions Get the pointer
+    // Get the raw pointer
     T* get() const noexcept {
         return ptr_;
     }
 
+    // Release ownership of the managed object
     T* release() noexcept {
         T* temp = ptr_;
         ptr_ = nullptr;
         return temp;
     }
 
+   // Reset the managed object with a new pointer (optional)
     void reset(T* ptr = nullptr) noexcept {
         delete ptr_;
         ptr_ = ptr;
     }
     
-    // Overload * operator
+    // Overload * operator and Dereference operator
     T& operator*() const noexcept {
         return *ptr_;
     }
 
-    // Overload -> operator
+    // arrow operator
     T* operator->() const noexcept {
         return ptr_;
     }
-    // this method is for to check the unique ptr is null or not
+    //This method is for to check the unique ptr is null or not
     explicit operator bool() const noexcept {
         return ptr_ != nullptr;
     }
 };
 
 int main (){
-  unique_ptr<int>ptr1(new int(2));
+
+  unique_ptr<int>ptr1(new int(2)); // Create a UniquePtr for an integer
+  std::cout << "Value: " << *ptr1 << std::endl;
+    
   unique_ptr<int>ptr2(new int(500));
   ptr2 = std:move(ptr1);
-  ptr1.get();
-  ptr1.reset(new int(30));
+  std::cout << "Value after move: " << *ptr2 << std::endl;  
+    
+ // p1 is now empty
+ if (!ptr1.get()) {
+    std::cout << "ptr1 is now empty" << std::endl;
+ }
+
+ // Reset ptr1 with a new value
+ ptr1.reset(new int(30));
+ std::cout << "New value in ptr1: " << *ptr1 << std::endl;
+
+ return 0;
 }
